79eb94fe9d96ad07e9482e44050996cfdf3e60e5
eric-tong <erictong.designs@gmail.com>
2020-03-02 16:11:53 +0000

    [Prediction] Remove plotting by default

--- src/predictiondelta/trainModels.js
+++ src/predictiondelta/trainModels.js
@@ -5,7 +5,6 @@ import "../service/config";
 import { getModel, trainModel } from "./model";
 
 import { getData } from "./data";
-import plotPredictionOnTrainingData from "./plotPredictionOnTrainingData";
 
 const tf = require("@tensorflow/tfjs-node");
 
@@ -15,7 +14,6 @@ const MODEL_SAMPLE_COUNT = 3;
 export async function start() {
   for (let tripId = 1; tripId <= 29; tripId++) {
     await trainAndSaveModel(tripId).catch(console.error);
-    await plotPredictionOnTrainingData(tripId);
   }
 }
 
@@ -37,6 +35,5 @@ async function trainAndSaveModel(tripId: number) {
       minLoss = loss;
     }
   }
-  console.log({ minLoss });
   if (model) model.save(`${MODEL_PATH}-${tripId}`);
 }
